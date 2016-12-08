9f59e8192115e918d721971a2348fe7a43ef26d1
job_talk <job-talk@users.noreply.github.com>
2016-12-08 01:43:28 +0100

    removed "reading" card from home, added sessionStorage for toggleable night-mode

--- js/night_mode.js
+++ js/night_mode.js
@@ -1,30 +1,31 @@
 // https://codepen.io/jamesbarnett/pen/kdDBL
 // "auto" night mode
+  $(document).ready(function(){
+  	var d = new Date();
+  	var n = d.getHours();
+  	if (n > 19 || n < 6)
+  	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
+  	  document.body.className = 'night';
+  	else
+  	  // Else use ‘day’ theme
+  	  document.body.className = 'day';
+  });
 
-$(document).ready(function(){
-	var d = new Date();
-	var n = d.getHours();
-	if (n > 19 || n < 6)
-	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
-	  document.body.className += ' night ';
-	else
-	  // Else use ‘day’ theme
-	  document.body.className += ' day ';
-});
 
-// toggleable inverter
-// from selfcare.tech
-document.addEventListener("DOMContentLoaded", function() {
-
-  document.getElementById('inverter').onclick = function() {
-
-    var className = ' ' + document.body.className + ' ';
-
-    if ( ~className.indexOf(' night ') ) {
-      document.body.className = className.replace(' night ', ' ');
+// toggleable inverter with sessionStorage
+// from http://stackoverflow.com/questions/28239337/store-a-css-class-on-browser
+  $('#inverter').click(function () {
+    $('body').toggleClass('day night');
+    if ($('body').hasClass('night')) {
+      sessionStorage.setItem('theme', 'night');
     } else {
-      document.body.className += ' night ';
+      sessionStorage.setItem('theme', 'day');
     }
+  });
 
-  };
-});
+  $(document).ready(function() {
+    var theme = sessionStorage.getItem('theme');
+    if (theme !== '') {
+      $('body').addClass(theme);
+    }
+  });
