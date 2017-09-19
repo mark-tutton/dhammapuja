be50f00adb30508b48be889b078d7a73f7eb08e0
job_talk <job-talk@users.noreply.github.com>
2017-09-19 17:44:37 -0500

    Fixed gulp build not minifying sass file

--- gulp/tasks/purifycss.js
+++ gulp/tasks/purifycss.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const purify = require('gulp-purifycss');
+const cssnano     = require('gulp-cssnano');
 
 /*
  * Removes unused CSS
@@ -8,5 +9,6 @@ const purify = require('gulp-purifycss');
 gulp.task('purify', ['scripts-prod', 'sass-prod'], function() {
   return gulp.src('app/assets/styles/main.css')
     .pipe(purify(['_site/assets/scripts/main.js', '_site/**/*.html']))
+    .pipe(cssnano())
     .pipe(gulp.dest('app/assets/styles/'));
 });
