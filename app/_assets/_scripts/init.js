46ec8e6165be1548b55f37357128e7f17e8050a5
job_talk <job-talk@users.noreply.github.com>
2018-02-21 19:34:21 -0600

    updated simply-search,

--- app/_assets/_scripts/init.js
+++ app/_assets/_scripts/init.js
@@ -19,17 +19,29 @@ var collapsibleInstance = M.Collapsible.init(collapsible, {
   accordion: false
 });
 
+var homeCollapsible = document.querySelector('.collapsible-home');
+var homeCollapsibleInstance = M.Collapsible.init(collapsible, {
+  accordion: false
+});
+
 var elem = document.querySelector('.fixed-action-btn');
 var instance = M.FloatingActionButton.init(elem);
 
 
 // Initialize the Jekyll Search plugin
 SimpleJekyllSearch({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
+  searchInput: document.querySelector('.search-input'),
+  resultsContainer: document.querySelector('.results-container'),
   json: '/search.json',
-  noResultsText: '<span id="search-no-results">No results found</span>'
+  noResultsText: '<span class="search-no-results">No results found</span>'
 });
+
+// SimpleJekyllSearch({
+//   searchInput: document.getElementById('mob-search-input'),
+//   resultsContainer: document.getElementById('mob-results-container'),
+//   json: '/search.json',
+//   noResultsText: '<span id="search-no-results">No results found</span>'
+// });
 
 // Initialize plyr
 plyr.setup('#track');
